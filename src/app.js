efc3e6a28197bf8fd984869a98cbf17e3e3ba558
Nitin Dixit <nitinajaydixit@gmail.com>
2022-03-04 12:42:26 +0530

    changed background image

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Sky",
+      animal: "Bird",
+      breed: "Budgie",
+    }),
+    React.createElement(Pet, {
+      name: "Dude",
+      animal: "Dog",
+      breed: "Desi",
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
