fefa245219ec523628a667054dec50285a9b9dda
Nitin Dixit <nitinajaydixit@gmail.com>
2022-03-25 06:22:30 +0530

    add comment

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -3,6 +3,6 @@ import { createContext } from "react";
 //const [theme, setTheme] = useState('darkblue');
 //mimicking/writing above hook as createContext func of react
 
-const ThemeContext = createContext(["green", () => {}]);
+const ThemeContext = createContext(["green", () => {}]);//this green will be overriden in app.js's useState
 
 export default ThemeContext;
